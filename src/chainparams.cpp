f68e8e76c7be607922d1c5dc960d3f3abd7730d1
John Ward <jward@flameport.com>
2023-03-11 14:46:19 +0000

    DNS seed addition
    3 tabby servers

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -169,6 +169,7 @@ public:
         // Note that of those with the service bits flag, most only support a subset of possible options
         vSeeds.push_back(CDNSSeedData("tabby.nyanseed.com", "tabby.nyanseed.com", true));
         vSeeds.push_back(CDNSSeedData("tabby.lakecharles.pictures", "tabby.lakecharles.pictures", true));
+        vSeeds.push_back(CDNSSeedData("tabby.katkoyn.org", "tabby.katkoyn.org", true));
 		
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,45);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
